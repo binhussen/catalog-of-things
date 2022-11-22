fe6bd725fcf752c73b782e0b18e708b3c35d7c4f
binhussen <mhdhsna@gmail.com>
2022-11-22 10:16:36 +0300

    Update main entry

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
-require './app'
+require_relative './task'
 
 def main
-  app = App.new
+  task = Task.new
   loop do
-    app.choose_task
+    task.choose_task
     input = gets.chomp.to_i
     if input == 10
       puts 'Thank you for using this app!'
       break
     end
-    app.do_task(input)
+    task.do_task(input)
   end
 end
 
